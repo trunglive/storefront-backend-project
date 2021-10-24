23a9ab3bc626882a6f883f8e078257a3b059dcdc
Trung Vo <trunglive@gmail.com>
2021-10-24 10:35:11 +0700

    feat: add initial product handler

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import productRoutes from "./handlers/product";
 
 const app: express.Application = express();
 const address: string = "0.0.0.0:3000";
@@ -9,6 +10,8 @@ app.use(bodyParser.json());
 app.get("/", function (req: Request, res: Response) {
   res.send("Hello World!");
 });
+
+productRoutes(app);
 
 app.listen(3000, function () {
   console.log(`starting app on: ${address}`);
