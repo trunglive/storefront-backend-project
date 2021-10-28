2b2f506a2ec8a9278b608d1bd8dfb8b7c76e6dd4
Trung Vo <trunglive@gmail.com>
2021-10-28 23:55:47 +0700

    fix: update order handler

--- src/handlers/order.ts
+++ src/handlers/order.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { Order, OrderStore } from "../models/order";
+import { Order, OrderProduct, OrderStore } from "../models/order";
 import verifyAuthToken from "./verifyAuthToken";
 
 const store = new OrderStore();
@@ -10,7 +10,7 @@ const index = async (_req: Request, res: Response) => {
 };
 
 const show = async (req: Request, res: Response) => {
-  const order = await store.show(req.body.id);
+  const order = await store.show(req.body.userId);
   res.json(order);
 };
 
@@ -29,13 +29,15 @@ const create = async (req: Request, res: Response) => {
   }
 };
 
-const addProduct = async (_req: Request, res: Response) => {
-  const orderId: string = _req.params.id;
-  const productId: string = _req.body.productId;
-  const quantity: number = parseInt(_req.body.quantity);
+const addProduct = async (req: Request, res: Response) => {
+  const orderProduct: OrderProduct = {
+    quantity: (req.headers.quantity as unknown) as number,
+    orderId: (req.headers.orderId as unknown) as number,
+    productId: (req.headers.productId as unknown) as number,
+  };
 
   try {
-    const addedProduct = await store.addProduct(quantity, orderId, productId);
+    const addedProduct = await store.addProduct(orderProduct);
     res.json(addedProduct);
   } catch (err) {
     res.status(400);
@@ -45,9 +47,9 @@ const addProduct = async (_req: Request, res: Response) => {
 
 const orderRoutes = (app: express.Application) => {
   app.get("/orders", index);
-  app.get("/orders/:id", show);
+  app.get("/orders/:userId", show);
   app.post("/orders", verifyAuthToken, create);
-  app.post("/orders/:id/products", verifyAuthToken, addProduct);
+  app.post("/orders/products", verifyAuthToken, addProduct);
 };
 
 export default orderRoutes;
