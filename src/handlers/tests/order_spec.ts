f35516e02d5f8fd8dcc55fa4c565f0859e09c82a
Trung Vo <trunglive@gmail.com>
2021-11-03 22:34:39 +0700

    fix: fix testing for order handler

--- src/handlers/tests/order_spec.ts
+++ src/handlers/tests/order_spec.ts
@@ -3,38 +3,41 @@ import app from "../../server";
 
 const request = supertest(app);
 
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRydW5ndm82IiwiaWF0IjoxNjM1OTUyMTcyfQ.d2OFXpnjwcSnkFDv9ZuFiMXgpvIr2WZIbEHaKummVeU";
+
 describe("Order Handler", () => {
-  it("should return success for READ all orders", async (done) => {
+  it("should return success for READ all orders", async () => {
     const response = await request.get("/orders");
 
     expect(response.status).toBe(200);
-
-    done();
+    expect(response.body).toBeTruthy();
   });
 
-  it("should return success for READ orders by user id", async (done) => {
+  it("should return success for READ orders by user id", async () => {
     const response = await request.get("/orders/1");
 
     expect(response.status).toBe(200);
-
-    done();
+    expect(response.body).toBeTruthy();
   });
 
-  it("should return success for CREATE order", async (done) => {
+  it("should return success for CREATE order", async () => {
     const response = await request
       .post("/orders")
+      .auth(token, { type: "bearer" })
       .send({ status: "ordered", userId: 1 });
 
     expect(response.status).toBe(200);
-
-    done();
+    expect(response.body).toBeTruthy();
   });
 
   it("should return success for CREATE order with product quantity and product id", async (done) => {
-    const response = await request.post("/orders/products");
+    const response = await request
+      .post("/orders/products")
+      .auth(token, { type: "bearer" })
+      .send({ quantity: 2, orderId: 1, productId: 2 });
 
     expect(response.status).toBe(200);
-
-    done();
+    expect(response.body).toBeTruthy();
   });
 });
